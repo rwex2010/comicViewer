7d94a7e1808cd05bb6dfb4b14750a4879475fe7f
Rich Wecks <richard.wecks@cdk.com>
2016-01-11 15:44:39 -0800

    todays work

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -3,11 +3,11 @@
 /* jasmine specs for controllers go here */
 describe('Comic controllers', function () {
 
+    // Load our app module definition before each test.
+    beforeEach(module('comicViewer'));
+
     describe('ComicListCtrl', function () {
         var scope, ctrl, $httpBackend;
-
-        // Load our app module definition before each test.
-        beforeEach(module('comicViewer'));
 
         beforeEach(inject(function (_$httpBackend_, $rootScope, $controller) {
             $httpBackend = _$httpBackend_;
@@ -34,5 +34,25 @@ describe('Comic controllers', function () {
         });
 
 
-    })
+    });
+
+    describe('ComicDetailCtrl', function () {
+        var scope, ctrl, $httpBackend;
+
+        beforeEach(inject(function (_$httpBackend_, $rootScope, $routeParams, $controller) {
+            $httpBackend = _$httpBackend_;
+            $httpBackend.expectGET('comics/xyz.json').respond({ComicName:'comic xyz'});
+
+            $routeParams.comicId='xyz';
+            scope = $rootScope.$new();
+            ctrl = $controller('ComicDetailCtrl', {$scope: scope});
+        }));
+
+        it('should fetch comic detail', function() {
+            expect(scope.comic).toBeUndefined();
+            $httpBackend.flush();
+
+            expect(scope.comic).toEqual({ComicName:'comic xyz'});
+        });
+    });
 });
