bef258f0809bb2f2dd73423795ec43d28bbe575b
Rich Wecks <richard.wecks@cdk.com>
2016-01-14 15:57:00 -0800

    add todays changes

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,21 +5,25 @@
 var ComicViewControllers = angular.module('ComicViewControllers', []);
 
 ComicViewControllers.controller('ComicListCtrl', ['$scope', '$http',
-function($scope, $http) {
-    $http.get('comics/comicsTemp.json').success(function(data){
-        $scope.comics=data;
-    });
+    function ($scope, $http) {
+        $http.get('comics/comicsTemp.json').success(function (data) {
+            $scope.comics = data;
+        });
 
-    $scope.orderProp = 'ComicName';
-}]);
+        $scope.orderProp = 'ComicName';
+    }]);
 
 ComicViewControllers.controller('ComicDetailCtrl', ['$scope', '$routeParams', '$http',
-function($scope, $routeParams, $http) {
-    $http.get('comics/' + $routeParams.comicId + '.json').success(function(data) {
-        //alert("paarams: " + $routeParams.comicId);
-        $scope.comic = data;
-        $scope.comic.mydate = "01/10/2017";
-    });
-    //$scope.comicId = $routeParams.comicId;
-}]);
+    function ($scope, $routeParams, $http) {
+        $http.get('comics/' + $routeParams.comicId + '.json').success(function (data) {
+            //alert("paarams: " + $routeParams.comicId);
+            $scope.comic = data;
+            $scope.mainImageUrl = data.images[0];
+            $scope.comic.mydate = "01/10/2017";
+        });
+
+        $scope.setImage = function (imageUrl) {
+            $scope.mainImageUrl = imageUrl;
+        };
+    }]);
 
