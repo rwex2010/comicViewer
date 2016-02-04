b56bc99d2bf08c3462e62d34c7aa8868d844f0db
Rich Wecks <richard.wecks@cdk.com>
2016-02-03 18:29:45 -0800

    Used controller to configure item url

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -20,20 +20,57 @@ ComicViewControllers.controller('ComicDetailCtrl', ['$scope', '$routeParams', 'C
         $scope.setImage = function (imageUrl) {
             $scope.mainImageUrl = imageUrl;
         };
-        comicViewer.config(function(ListComicProvider) {
-            console.log("in the config function in controller");
-            ListComicProvider.setComicId("Zits");
-        });
 
     }]);
 
-//ComicViewControllers.controller('SomeDetailCtrl', function ($scope, ListComic) {
-comicViewer.controller('SomeDetailCtrl', function ($scope, ListComic) {
-    //var comicId = "Zits";
-    this.comicStrips = ListComic;
+////ComicViewControllers.controller('SomeDetailCtrl', function ($scope, ListComic) {
+//comicViewer.controller('SomeDetailCtrl', function ($scope, ListComic) {
+//    //var comicId = "Zits";
+//    this.comicStrips = ListComic;
+//
+//    //this.comicStrips = function(comicId) {
+//    //    //return ListComic(comicId);
+//    //  return  ListComic(comicId);
+//    //}
+//});
+
+comicViewer.controller('SomeDetailCtrl',['$scope', '$routeParams', 'ListComic',  function ($scope,$routeParams, ListComic) {
+    var comicId = $routeParams.comicId;
+    return {
+        comicStrips: GetImages(comicId)
+    };
+
+   function GetImages(comicId) {
+        var imageLocation = "img/comicStrips";
+        var newComicId = comicId;
+
+        console.log("in the get function: " + newComicId);
+        //return function (newComicId) {
+        console.log("in the return function: " + newComicId);
+        var returnValue = [];
+
+        var strtDate = new Date();
+        var toDay = strtDate;
+        for (var days = 0; days < 14; days++) {
+            var strYear = toDay.getFullYear();
+            var strMon = ( "0" + (toDay.getMonth() + 1)).slice(-2);
+            var strDay = ( "0" + toDay.getDate()).slice(-2);
+            //returnValue[days] = ComicKey+strYear+strMon+strDay;
+            var imageId = newComicId + strYear + strMon + strDay + ".jpg";
+            returnValue[days] = imageLocation + "/" + newComicId + "/" + imageId;
+
+            toDay.setDate(toDay.getDate() - 1);
+        }
+        console.log("before the return: " + returnValue.length);
+
+        return returnValue;
+
+        //}
+    }
+
     //this.comicStrips = function(comicId) {
     //    //return ListComic(comicId);
     //  return  ListComic(comicId);
     //}
-});
+}]);
 
