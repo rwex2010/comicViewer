6d6bcd0c4d08ec9a3d60f3bb6ea281c17d4a1107
Rich Wecks <rich_wecks@hotmail.com>
2016-05-04 17:55:12 -0700

    loading same data in both sets of input boxes

--- app/js/SetupDirective/comicSetupController.js
+++ app/js/SetupDirective/comicSetupController.js
@@ -7,85 +7,121 @@
 ComicViewControllers.controller('comicSetupCtrl', ['$q', '$scope', 'readMyJasonService', 'filterFilter', comicSetup]);
 
 function comicSetup($q, $scope, readMyJasonService, filterFilter) {
-    var vm = this;
-    var deferred = $q.defer();
-    var comicToUseObj;
+    // var vm = this;
+    var deferred1;
+    var deferred2;
+    // var comicToUseObj;
     var activeUrl;
     var jsonUrl;
     var aryWhichOne = ["Raw", "Formatted"];
     var rawUrl = 'comics/allComics.json';
     var formattedUrl = 'comics/comicTest.json';
     var aryJsonUrl = [rawUrl, formattedUrl];
+    $scope.jsonUrl1 = rawUrl;
+    $scope.formattedUrl = formattedUrl;
     var blnCapturedRaw = false;
-    // var jsonUrl =  'comics/comicTest.json';
-    // var jsonUrl =  'comics/comicsNew.json';
-    // var jsonUrl =  'comics/comics.json';
-    // var jsonUrl =  'comics/allComics.json';
-    // for (var ix = 0; ix < 1; ix++) {
-    var ix = 1;
+    var ix = 0;
+    // var ix = 1;
+    // for (var ix = 0; ix < 2; ix++) {
+    //     if (ix == 0) {
+            deferred1 = $q.defer();
+        // }
+        // if (ix == 1) {
+        //     deferred2 = $q.defer();
+        // }
+
         activeUrl = aryWhichOne[ix];
         jsonUrl = aryJsonUrl[ix];
 
         // while (!blnCapturedRaw) {
 
-            // activeUrl = aryWhichOne[0];
-            // jsonUrl = aryJsonUrl[0];
-            // readMyJasonService.getMyJason($routeParams.comicName)
             readMyJasonService.getMyJason(jsonUrl)
                 .then(getJasonSuccess)
                 .catch(errorCallBack);
+    // }
+    function getJasonSuccess(data) {
+            deferred1.resolve(data)
+            getJsonRaw(data);
+        if (!data) {
+            console.log("Data is empty");
+            return;
+        }
+            // deferred2 = $q.defer();
+        readMyJasonService.getMyJason(aryJsonUrl[1])
+            .then(getJasonFormattedResolve)
+            .catch(errorCallBack);
 
-            function getJasonSuccess(data) {
-                deferred.resolve(data)
-                if (!data) {
-                    console.log("Data is empty");
-                    return;
-                }
-                if (activeUrl == "Raw") {
-                    getJsonRaw(data);
-                } else if (activeUrl == "Formatted") {
+        function getJasonFormattedResolve() {
+            deferred1.resolve(data)
                     getJsonFormatted(data);
-                }
+            if (!data) {
+                console.log("Data is empty");
+                return;
             }
+
+        }
+
+        // if (activeUrl == "Raw") {
+        //     getJsonRaw(data);
+        // } else if (activeUrl == "Formatted") {
+        //     getJsonFormatted(data);
+        // }
+    }
+
+            // function getJasonSuccess(data) {
+            //     if (data.length > 90) {
+            //         deferred1.resolve(data)
+            //         getJsonRaw(data);
+            //     }
+            //     if (data.length < 90) {
+            //         deferred2.resolve(data)
+            //         getJsonFormatted(data);
+            //     }
+            //     if (!data) {
+            //         console.log("Data is empty");
+            //         return;
+            //     }
+            //     // if (activeUrl == "Raw") {
+            //     //     getJsonRaw(data);
+            //     // } else if (activeUrl == "Formatted") {
+            //     //     getJsonFormatted(data);
+            //     // }
+            // }
         // }
 
-    // }
-
     function getJsonRaw(data) {
-        $scope.jsonUrl = jsonUrl;
         $scope.theseComics = data;
         $scope.selectedComic = $scope.theseComics[0];
-        $scope.ComicImgCode = $scope.selectedComic.ComicCode;
-        $scope.DaysAvailable = $scope.selectedComic.DaysAvailable;
-        $scope.Description = $scope.selectedComic.description;
-        $scope.ImageCode = $scope.selectedComic.imgCode;
-        $scope.Domain = $scope.selectedComic.Domain;
-        $scope.ImageDirectory = $scope.selectedComic.imgDirectoryLocation;
-        if ($scope.selectedComic.OneOfMyComics) {
-            $scope.OneOfMine = "TRUE";
-        } else {
-            $scope.OneOfMine = "FALSE";
-        }
-        $scope.Priority = $scope.selectedComic.priority;
+        // $scope.ComicImgCode = $scope.selectedComic.ComicCode;
+        // $scope.DaysAvailable = $scope.selectedComic.DaysAvailable;
+        // $scope.Description = $scope.selectedComic.description;
+        // $scope.ImageCode = $scope.selectedComic.imgCode;
+        // $scope.Domain = $scope.selectedComic.Domain;
+        // $scope.ImageDirectory = $scope.selectedComic.imgDirectoryLocation;
+        // if ($scope.selectedComic.OneOfMyComics) {
+        //     $scope.OneOfMine = "TRUE";
+        // } else {
+        //     $scope.OneOfMine = "FALSE";
+        // }
+        // $scope.Priority = $scope.selectedComic.priority;
         // blnCapturedRaw = true;
     }
 
     function getJsonFormatted(data) {
-        $scope.formattedUrl = jsonUrl;
-        $scope.formattedComics = data;
-        $scope.selectedFormattedComics = $scope.formattedComics[2];
-        $scope.FormattedComicImgCode = $scope.selectedFormattedComics.ComicCode;
-        $scope.FormattedDaysAvailable = $scope.selectedFormattedComics.DaysAvailable;
-        $scope.FormattedDescription = $scope.selectedFormattedComics.description;
-        $scope.FormattedImageCode = $scope.selectedFormattedComics.imgCode;
-        $scope.FormattedDomain = $scope.selectedFormattedComics.Domain;
-        $scope.FormattedImageDirectory = $scope.selectedFormattedComics.imgDirectoryLocation;
-        if ($scope.selectedFormattedComics.OneOfMyComics) {
-            $scope.FormattedOneOfMine = "TRUE";
-        } else {
-            $scope.FormattedOneOfMine = "FALSE";
-        }
-        $scope.FormattedPriority = $scope.selectedFormattedComics.priority;
+        $scope.theseComics2 = data;
+        $scope.selectedComic2 = $scope.theseComics2[2];
+        // $scope.ComicImgCode2 = $scope.selectedComic2.ComicCode;
+        // $scope.DaysAvailable2 = $scope.selectedComic2.DaysAvailable;
+        // $scope.Description2 = $scope.selectedComic2.description;
+        // $scope.imgCode2 = $scope.selectedComic2.imgCode;
+        // $scope.Domain2 = $scope.selectedComic2.Domain;
+        // $scope.ImageDirectory2 = $scope.selectedComic2.imgDirectoryLocation;
+        // if ($scope.selectedComic2.OneOfMyComics) {
+        //     $scope.FormattedOneOfMine = "TRUE";
+        // } else {
+        //     $scope.FormattedOneOfMine = "FALSE";
+        // }
+        // $scope.FormattedPriority = $scope.selectedComic2.priority;
     }
 
     function errorCallBack() {
